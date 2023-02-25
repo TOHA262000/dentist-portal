216043b2b403ba55ad805734cefe956a01c5e649
TOHA262000 <m.i.toaha14@gmail.com>
2023-02-25 16:48:59 +0600

    Dashboard Complete

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,41 +1,55 @@
 import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import React, { useContext, useState } from 'react';
+import { DayPicker } from 'react-day-picker';
 import { Link } from 'react-router-dom';
-import PrimaryButton from '../../components/PrimaryButton';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import TableRow from './TableRow';
 
 const Dashboard = () => {
     const { user } = useContext(AuthContext);
     const [date, setDate] = useState(new Date());
+    const [showCalender, setShowCalender] = useState(false);
     const formatedDate = format(date, 'PP');
     const { data: bookings = [] } = useQuery({
-        queryKey: ['bookings',formatedDate,user.email],
-        queryFn: async() => {
+        queryKey: ['bookings', formatedDate, user.email],
+        queryFn: async () => {
             const res = await fetch(`http://localhost:5000/bookings?formatedDate=${formatedDate}&email=${user.email}`, {
                 method: 'get',
                 headers: {
-                    "Content-Type": "application/json",  
+                    "Content-Type": "application/json",
                 },
             });
             const data = await res.json();
             return data;
-                
+
         }
     })
-    console.log(bookings)
+    const handleCalender=()=>{
+        setShowCalender(!showCalender);
+    }
     return (
-        <div className="drawer drawer-mobile">
+        <div className="relative drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content bg-base-300 p-5">
 
 
                 <div className='flex justify-between my-4'>
                     <h1 className='text-3xl' >My appointment</h1>
-                    <button className='btn btn-outline'>{formatedDate}</button>
+
+                    <div>
+                        <label onClick={handleCalender} className="btn btn-outline">{formatedDate}</label>
+                        <div className='bg-gray-200 p-4 mt-4 rounded absolute top-20 right-4 z-10 right-0'>
+                            {showCalender && <DayPicker
+                                mode='single'
+                                selected={date}
+                                onSelect={setDate}
+                            />}
+                        </div>
+
+                    </div>
                 </div>
-                <div className="overflow-x-auto">
+                <div className=" overflow-x-auto z-0">
                     <table className="table w-full">
                         <thead>
                             <tr>
@@ -46,10 +60,10 @@ const Dashboard = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            { bookings.map((booking,i) => <TableRow 
+                            {bookings.map((booking, i) => <TableRow
                                 key={booking._id}
-                                booking = {booking}
-                                index = {i}
+                                booking={booking}
+                                index={i}
                             ></TableRow>)}
                         </tbody>
                     </table>
@@ -57,7 +71,7 @@ const Dashboard = () => {
 
 
 
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                <label htmlFor="my-drawer-2" className="btn btn-outline drawer-button lg:hidden">Side Nav</label>
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
